9efb9693f921638cfad324a62ddba2f5e5985b4a
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-03-28 14:26:18 +0530

    python script added

---
+++ V3/script.py
@@ -0,0 +1,21 @@
+import pyautogui
+#import webbrowser as wb
+import time
+#time.sleep(5)
+#f = open("baby lyrics", 'r')
+#wb.open("web.whatsapp.com")
+#for word in f:
+#   pyautogui.typewrite(word)
+
+#  pyautogui.press("enter")
+time.sleep(15)
+for i in range(100):
+    pyautogui.press("H")
+    pyautogui.press("i")
+    pyautogui.press("enter")
+    pyautogui.press("t")
+    pyautogui.press("h")
+    pyautogui.press("e")
+    pyautogui.press("r")
+    pyautogui.press("e")
+    pyautogui.press("enter")
